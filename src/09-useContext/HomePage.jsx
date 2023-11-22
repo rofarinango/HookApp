4636e0782ce6c36c4a69e484169dd05f69d8bc02
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-11-22 15:15:22 -0600

    Pruebas en HomePage

--- src/09-useContext/HomePage.jsx
+++ src/09-useContext/HomePage.jsx
@@ -4,11 +4,12 @@ import { UserContext } from './context/UserContext'
 export const HomePage = () => {
 
   const {user} = useContext(UserContext);
+
   return (
     <>
         <h1>HomePage <small>{user?.name}</small></h1>
         <hr />
-        <pre>
+        <pre aria-label='pre'>
           {JSON.stringify(user, null, 3)}
         </pre>
 
